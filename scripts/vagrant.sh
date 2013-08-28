5d15fd6f9d6ab1126251f2863624c9ad8011e78c
PeteMS <p.morsou@gmail.com>
2013-08-28 15:32:08 +0100

    Update vagrant.sh

--- scripts/vagrant.sh
+++ scripts/vagrant.sh
@@ -7,6 +7,6 @@ apt-get update
 apt-get install git puppet -y
 cd /etc/puppet
 git init
-git remote add origin https://github.com/petems/headless-puppet-rbenv
+git remote add origin https://github.com/petems/headless-puppet-ruby
 git pull -u origin master
 puppet apply /etc/puppet/manifests/default.pp --modulepath /etc/puppet/modules
